e500c444372ab317c29e421c77e60ab5c8195ef4
Orca <secretservicefrog@gmail.com>
2026-02-20 10:42:49 -0500

    Commit to resolve merge conflicts.

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Earthlings!</h1>
+    <div class="card">
+    </div>
+    <p class="read-the-docs">
+      Welcome Humans 
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
